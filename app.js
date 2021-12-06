dddb3edad0b188ba10953d93a2cbf1b7042d89df
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-07 02:22:15 +0530

    error handling

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const birthDate = document.querySelector("#birth-date");
 var luckyNo = document.querySelector("#lucky-No");
 var checkButton = document.querySelector("#check-Button");
-var message = document.querySelector("#message")
+var message = document.querySelector("#message");
 
-const noArray = [];
-checkButton.addEventListener( "click",checkLuckyOne);
+checkButton.addEventListener("click", checkLuckyOne);
 
-function checkLuckyOne(){
- const dateofBirth = birthDate.value
-doB = dateofBirth.split("-"); 
-const year = Number(doB[0]);
-const Month = Number(doB[1]);
-const day = Number(doB[2]);
+function checkLuckyOne() {
+  const dateofBirth = birthDate.value;
+  doB = dateofBirth.split("-");
+  const year = Number(doB[0]);
+  const Month = Number(doB[1]);
+  const day = Number(doB[2]);
 
-var sum = (year)+(Month)+(day);
-console.log(sum);
-console.lo
-message.innerText ="";
+  var sum = year + Month + day;
 
-if (sum%luckyNo.value==0)
-{
-message.innerText = "ohhh!!!....your Date of birth is lucky😊"
-}
-else{
-    message.innerText ="Not a lucky one☹️...just for fun"
-}
+  if (sum % luckyNo.value == 0) {
+    message.innerText = "ohhh!!!....your Date of birth is lucky😊";
+  } else {
+    message.innerText = "Not a lucky one☹️...just for fun";
+  }
 }
