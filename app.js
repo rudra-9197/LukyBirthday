00f1f08120c6547018f09de73d609a99a27d9e4d
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-07 13:45:00 +0530

    logic Change

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ var message = document.querySelector("#message");
 checkButton.addEventListener("click", checkLuckyOne);
 
 function checkLuckyOne() {
-  const dateofBirth = birthDate.value;
-  doB = dateofBirth.split("-");
-  const year = Number(doB[0]);
-  const Month = Number(doB[1]);
-  const day = Number(doB[2]);
-
-  var sum = year + Month + day;
-
+  var dateofBirth = birthDate.value;
+  dateMonthYear = dateofBirth.replaceAll("-", "");
+  doB = dateMonthYear.split("");
+  const sum = calculateSum(doB);
   if (sum % luckyNo.value == 0) {
     message.innerText = "ohhh!!!....your Date of birth is lucky😊";
-  } else {
-    message.innerText = "Not a lucky one☹️...just for fun";
-  }
+  } 
+  else {    message.innerText = "Not a lucky one☹️...just for fun";
+   }
+ 
 }
+
+function calculateSum(doB) {
+  let sum = 0;
+  for (let i = 0; i < doB.length; i++) {
+    sum += Number(doB[i]);
+  }
+
+  return sum;
+}
+
+//doB = dateofBirth.split("-");
+//console.log(doB);
+//const year = Number(doB[0]);
+//const Month = Number(doB[1]);
+//const day = Number(doB[2]);
+
+// var sum = year + Month + day;
+
+
